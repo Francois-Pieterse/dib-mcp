a602f22dec6ca974880983071f54448076df500b
Francois Pieterse <francois.pieterse@potch.matogen.com>
2025-12-17 11:28:23 +0200

    Added other example resources.

---
+++ server/resources/other_resources.py
@@ -0,0 +1,15 @@
+from mcp_instance import mcp
+
+# To expose these resources, import this module in main.py
+
+@mcp.resource(
+    uri="dib://example/resource",
+    name="example_resource",
+    title="Example Resource",
+    description="This is an example resource.",
+)
+def example_resource():
+    return {
+        "content_type": "text/plain",
+        "body": "This is the content of the example resource.",
+    }
