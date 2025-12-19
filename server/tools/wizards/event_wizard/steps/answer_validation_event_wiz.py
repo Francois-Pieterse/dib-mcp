2c84e1f8edf96259b2dbab31e29944a277aec3ac
Francois Pieterse <francois.pieterse@potch.matogen.com>
2025-12-19 09:11:06 +0200

    Added class name validation.

--- server/tools/wizards/event_wizard/steps/answer_validation_event_wiz.py
+++ server/tools/wizards/event_wizard/steps/answer_validation_event_wiz.py
@@ -9,9 +9,39 @@ from tools.wizards.base.validation_base import (
     validate_step_answers as base_validate,
 )
 
+# Event Wizard specific validators
+def _validate_class_name(
+    field: FieldCfg, value: Any, errors: list[ValidationError]
+) -> bool:
+    name = field["name"]
+    if not isinstance(value, str):
+        _add_error(errors, name, f"'{name}' must be a string value.")
+        return
 
-# WIZARD_SPECIFIC_TYPE_VALIDATORS: dict[str, Callable] = {
-# }
+    # Ensure the class name starts with a letter, preferably uppercase, and contains only valid characters
+    pattern = r"^[A-Z][A-Za-z0-9_]*$"
+    if not re.match(pattern, value):
+        _add_error(
+            errors,
+            name,
+            (
+                "Class name must start with an uppercase letter and contain only "
+                "letters, digits, and underscores, with no spaces."
+            ),
+        )
+
+    # Class name must end with 'Controller.php'
+    if not value.endswith("Controller.php"):
+        _add_error(
+            errors,
+            name,
+            "Class name must end with 'Controller.php'. e.g. MyNewController.php",
+        )
+
+
+WIZARD_SPECIFIC_TYPE_VALIDATORS: dict[str, Callable] = {
+    "class_name": _validate_class_name,
+}
 
 
 # Wrapper function to combine base validation with wizard-specific validators
@@ -25,5 +55,5 @@ def validate_step_answers(
     return base_validate(
         step_cfg,
         answers,
-        type_validators=None,
+        type_validators=WIZARD_SPECIFIC_TYPE_VALIDATORS,
     )
